153e4d798002b4c1a729a1a2598380512f44debf
Sergiy <sergiy_stebelskiy@MacBook-Air-Sergiy.local>
2021-09-05 21:24:47 +0300

    added validation for Task model

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,17 @@
-export class CreateTaskDto {}
+import { IsNotEmpty, IsString, IsBoolean } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsBoolean()
+  checked: boolean;
+
+  @IsNotEmpty()
+  @IsString()
+  priority: string;
+}
