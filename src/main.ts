0d3fecdade8a7658eae7c519cdf043061fe622b7
SergiyStebelskiy <sergiy.stebelskiy.01@gmail.com>
2021-10-09 21:41:28 +0300

    changed port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
 		logger: ['error', 'warn']
 	})
-	// app.useGlobalPipes(new ValidationPipe())
-	await app.listen(3000)
+	await app.listen(8080)
 }
 bootstrap()
